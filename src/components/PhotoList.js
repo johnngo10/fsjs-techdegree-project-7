99b6bbd1ee135f87a2cc291c1b1626b59f885b16
xuanngo10 <xuan.ngo10@gmail.com>
2020-05-04 08:33:00 -0700

    displayed images

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -3,14 +3,17 @@ import Photo from "./Photo";
 
 const PhotoList = (props) => {
   const results = props.data;
-  let photos = results.map((photo) => <Photo />);
+  const farmId = props.data.farm;
+  let photos = results.map((photo) => (
+    <Photo
+      url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+    />
+  ));
 
   return (
     <div className="photo-container">
       <h2>Results</h2>
-      <ul>
-        <Photo />
-      </ul>
+      <ul>{photos}</ul>
     </div>
   );
 };
